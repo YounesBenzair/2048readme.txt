ff2f773040a876a6cf94228622408fca76be23b3
YounesBenzair <90462072+YounesBenzair@users.noreply.github.com>
2022-04-13 09:31:41 +0200

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,33 @@
+#import
+import randum 
+import tkinter
+
+# création des case
+print("\033[1m")
+# Couleur des case
+rouge =  "\033[91m"
+vert =  "\033[92m"
+violet = "\033[95m"
+blanc =  "\033[97m"
+orange = "\033[38;2;255;164;0m"
+rose = "\033[38;2;255;192;203m"
+noir = "\033[38;2;0;0;0m"
+gris = "\033[38;2;220;220;220m"
+
+
+#creation de tout les chiffres
+
+
+#cree une def gagné
+#cree une def perdu
+
+
+
+
+
+
+
+
+
+
+#incremente les touche pour se déplacer zqsd ou flèche
